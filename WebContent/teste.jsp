5d3b671cf1c47ac9d468791f1394c836e06262bc
everton26se <everton26se@gmail.com>
2014-05-07 17:50:56 -0300

    Login servlet e login.jsp

--- WebContent/teste.jsp
+++ WebContent/teste.jsp
@@ -2,14 +2,13 @@
 <%@taglib prefix="t" tagdir="/WEB-INF/tags" %>
 
 <t:layout>
-    <jsp:attribute name="header">
-      <h1>Welcome</h1>
+    <jsp:attribute name="titulo">
+      Cadastrar cliente
     </jsp:attribute>
-    <jsp:attribute name="footer">
-      <p id="copyright">Copyright 1927, Future Bits When There Be Bits Inc.</p>
-    </jsp:attribute>
+    
     <jsp:body>
-        <p>Everton alterou aqui</p>
-        <p>Emanoel alterou aqui</p>
+        
+        asdfsdafja;sldfjkasdf k;adfjs;kadfjs 
     </jsp:body>
+    
 </t:layout>
---
+++ src/com/everton/cookmyfood/Servlets/CadastroCategoriaServlet.java
@@ -0,0 +1,33 @@
+package com.everton.cookmyfood.Servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.everton.cookmyfood.Aplicacao.CategoriaAplicacao;
+import com.everton.cookmyfood.Models.Categoria;
+
+@WebServlet("/cadastro-categoria.do")
+public class CadastroCategoriaServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    public CadastroCategoriaServlet() {
+        super();
+        
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {	
+		
+		Categoria c = new Categoria();
+		c.setNome("refrigerantes");
+
+		CategoriaAplicacao app = new CategoriaAplicacao();
+		app.salvar(c);
+		
+	}
+
+}
